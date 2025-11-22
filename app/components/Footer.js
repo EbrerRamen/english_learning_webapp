7188903e3d5770d6e965b00e6f6ee4742dda617f
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-11-22 21:14:55 +0600

    Footer Content

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-gray-700 py-6 mt-10 text-center">
+      <p>© 2025 EnglishApp. All rights reserved.</p>
+    </footer>
+  );
+}
